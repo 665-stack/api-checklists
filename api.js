a220bd27390923e22c4c81960395f467b4b943de
unknown <mahmodulshahin665@gmail.com>
2022-05-13 18:58:11 +0600

    add checklist ans 8 & 9

--- api.js
+++ api.js
@@ -5,15 +5,37 @@ const loadPic = () => {
 }
 loadPic()
 
-const displayPic = users => {
+const displayPic = images => {
     const userContainer = document.getElementById('users');
-    users.forEach(user => {
+    images.forEach(img => {
         const div = document.createElement('div');
-        div.classList.add('user');
         div.innerHTML = `
-            <img src="${user.url}" alt="">
-            <h4> Title: ${user.title}</h4>
+           <div class="user" onclick = "loadImgDetails(${img.id})">
+              <img src="${img.url}" alt="">
+              <h4 class="user-title"> Title: ${img.title}</h4>
+           </div>
     `;
         userContainer.appendChild(div);
     });
 }
+const loadImgDetails = imgId => {
+    const url = `https://jsonplaceholder.typicode.com/photos/${imgId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayImgDetails(data))
+}
+const displayImgDetails = perImg => {
+    // console.log('perImg -- ', perImg.title)
+    const imgDetails = document.getElementById('img-datails');
+    //clear previous perImg details
+    imgDetails.textContent = '';
+
+    const div = document.createElement('div');
+    div.classList.add('imgDetails');
+    div.innerHTML = `   
+    <img src="${perImg.url}" alt="">
+    <img src="${perImg.thumbnailUrl}" alt="">
+    <h4 class="user-title"> Title: ${perImg.title}</h4>
+    `;
+    imgDetails.appendChild(div)
+} 
